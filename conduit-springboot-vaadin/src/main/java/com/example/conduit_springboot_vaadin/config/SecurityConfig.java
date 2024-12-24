21ba455ea033813f30f594c9af87036df3b7a7fb
Marko Milunovic <markomilunovic9@gmail.com>
2024-12-24 19:08:56 +0100

    feat: add tags endpoint
    - Created TagController.java with the /api/tags endpoint.
    - Created TagService.java to handle the business logic for fetching distinct tags.
    - Created TagsResponseDto.java to structure the response payload for the tags.

--- conduit-springboot-vaadin/src/main/java/com/example/conduit_springboot_vaadin/config/SecurityConfig.java
+++ conduit-springboot-vaadin/src/main/java/com/example/conduit_springboot_vaadin/config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                                 "/api/articles/*",
                                 "/api/articles/*/comments",
                                 "/api/articles",
+                                "/api/tags",
                                 "/api/profiles/**",
                                 "/api/profiles"
                         ).permitAll()
